2f019a4d4759351408afc28bdc5990b6069eae45
yukihane <yukihane.feather@gmail.com>
2022-08-24 05:04:52 +0900

    一旦 commit

--- mavenize.sh
+++ mavenize.sh
@@ -6,3 +6,5 @@ curl -L http://www.oreilly.co.jp/pub/9784873115320/tpdsl-code.tgz | tar xz
 
 mv -v code/parsing/lexer/*.java parsing/lexer/src/main/java/
 
+mv -v code/parsing/recursive-descent/*.java parsing/recursive-descent/src/main/java/
+mv -v code/parsing/recursive-descent/*.g parsing/recursive-descent/src/main/antlr3/
