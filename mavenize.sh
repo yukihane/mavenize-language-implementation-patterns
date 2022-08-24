e9182e0b5dbaf88a8087ece7f9395cb49c79a379
yukihane <yukihane.feather@gmail.com>
2022-08-25 04:24:42 +0900

    parsing/multi

--- mavenize.sh
+++ mavenize.sh
@@ -8,3 +8,6 @@ mv -v code/parsing/lexer/*.java parsing/lexer/src/main/java/
 
 mv -v code/parsing/recursive-descent/*.java parsing/recursive-descent/src/main/java/
 mv -v code/parsing/recursive-descent/*.g parsing/recursive-descent/src/main/antlr3/
+
+mv -v code/parsing/multi/*.java parsing/multi/src/main/java/
+mv -v code/parsing/multi/*.g parsing/multi/src/main/antlr3/
